bc58fca12767d95ed9bb27225aad18c8db21e387
Lê Đức Anh Phương <127871368+KICHm1@users.noreply.github.com>
2023-07-31 19:15:07 +0700

    fix json

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-<<<<<<< HEAD
 module.exports = {
     exportTrailingSlash: true,
   }
-=======
-const nextConfig = {}
-
-module.exports = nextConfig
-
->>>>>>> e79bc2d72e1615fdbfd7dccbb4d70a4c71205dad
