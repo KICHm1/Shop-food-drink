6c7ada52da825d55d03df8809c059f240c2cbf9e
Lê Đức Anh Phương <127871368+KICHm1@users.noreply.github.com>
2023-07-31 19:27:50 +0700

    fix web

--- pages/api/source/index.tsx
+++ pages/api/source/index.tsx
@@ -1,5 +1,4 @@
 import { NextApiRequest, NextApiResponse } from "next";
-<<<<<<< HEAD
 import { main } from "../main";
 export default function Handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "GET") {
@@ -24,18 +23,4 @@ export default function Handler(req: NextApiRequest, res: NextApiResponse) {
     });
     res.status(201).json(contents);
   }
-=======
-
-export default function Handler(req: NextApiRequest, res: NextApiResponse) {
-  res.json([
-    { banner : false , popular : true, check: "food", url: "images/food-bowl.jpg", id: "Bowl", detail : "Đây là những món ngon tuyệt vời", money: "20000", name : "Bowl" },
-    { banner : false , popular : false , check: "food", url: "images/food-macarons.jpg", id: "Macarons", detail : "Đây là những món ngon tuyệt vời", money: "25000", name : "Macarons" },
-    { banner : false , popular : false , check: "food", url: "images/food-miaso.jpg", id: "Miaso", detail : "Đây là những món ngon tuyệt vời", money: "30000", name : "Miaso" },
-    { banner : true , popular : true , check: "food", url: "images/food-pizza.jpg", id: "Pizza", detail : "Đây là những món ngon tuyệt vời", money: "22000", name : "Pizza" },
-    { banner : true , popular : false , check: "drink", url: "images/drink-coffe.jpg", id: "Coffe", detail : "Đây là những món ngon tuyệt vời", money: "25000", name : "Coffe" },
-    { banner : false , popular : false , check: "drink", url: "images/drink-cold-summer.jpg", id: "Cold-Summer", detail : "Đây là những món ngon tuyệt vời", money: "17000", name : "Cold Summer" },
-    { banner : true , popular : false, check: "drink", url: "images/drink-fruit.jpg", id: "Fruit", detail : "Đây là những món ngon tuyệt vời", money: "25000", name : "Fruit" },
-    { banner : true , popular : true, check: "drink", url: "images/drink-milk.jpg", id: "Milk", detail : "Đây là những món ngon tuyệt vời", money: "28000", name : "Milk" },
-  ]);
->>>>>>> e79bc2d72e1615fdbfd7dccbb4d70a4c71205dad
 }
